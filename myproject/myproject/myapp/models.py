4aecfe0e5c16e97337c8803293f2cb21878d220e
anaconda0 <omarmedhatmoheb@gmail.com>
2024-08-07 13:25:46 +0300

    Add nested myproject folder with initial setup

--- myproject/myproject/myapp/models.py
+++ myproject/myproject/myapp/models.py
@@ -17,15 +17,12 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
     model_number = models.CharField(max_length=255)
-    category = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    warranty = models.CharField(max_length=255)
     color = models.CharField(max_length=255)
     weight = models.FloatField()
     dimensions = models.CharField(max_length=255)
     images = models.ImageField(upload_to='products/')
     description = models.TextField()
-    manufacturer_part_number = models.CharField(max_length=255)
     specification = models.ForeignKey(Specification, on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
