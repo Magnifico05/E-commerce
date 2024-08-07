3ea56d977180b60c9576371214f2b5d64728ab60
alikhattab <ali-khattab@aucegypt.edu>
2024-08-07 15:12:32 +0300

    registered models to admins page

--- myproject/myproject/myapp/admin.py
+++ myproject/myproject/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Address, Specification, Product, categories, order, orderitem, cart, cartitem
 
-# Register your models here.
+
+myModels = [ User, Address, Specification, Product, categories, order, orderitem, cart, cartitem]  # iterable list
+for model in myModels:
+    admin.site.register(model)
--- myproject/myproject/myapp/models.py
+++ myproject/myproject/myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractBaseUser, BaseUserManager
+
 class Address(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     country = models.CharField(max_length=255)
