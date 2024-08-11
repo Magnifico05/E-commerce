976ef8e9e4093cb143dd7d6159d7d57f1c6ecb39
anaconda0 <omarmedhatmoheb@gmail.com>
2024-08-11 12:27:50 +0300

    Resolved merge conflicts

--- myproject/myproject/myapp/views.py
+++ myproject/myproject/myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Specification
+from .serializers import SpecificationSerializer
 
-# Create your views here.
+class ProductSpecificationListView(generics.ListAPIView):
+    serializer_class = SpecificationSerializer
+
+    def get_queryset(self):
+        product_id = self.kwargs.get('product_id')
+        print(f"Product ID in get_queryset: {product_id}")  # Debug statement
+        return Specification.objects.filter(product_id=product_id)
