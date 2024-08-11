02ac0fc7dcafdc5a3fc97b4bc65d5042378d9b93
alikhattab <ali-khattab@aucegypt.edu>
2024-08-11 11:33:41 +0300

    added an API to retrieve user addresses

--- myproject/myproject/myapp/views.py
+++ myproject/myproject/myapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Address
+from .serializers import AddressSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class UserAddressesView(generics.ListAPIView):
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return Address.objects.filter(user_id=user_id)
+
+
--- myproject/myproject/myproject/urls.py
+++ myproject/myproject/myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import UserAddressesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/<int:user_id>/addresses/', UserAddressesView.as_view(), name='user-addresses'),
 ]
